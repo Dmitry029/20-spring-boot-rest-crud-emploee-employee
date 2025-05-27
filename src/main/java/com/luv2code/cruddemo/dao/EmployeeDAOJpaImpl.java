2e1bdc8fcb2e68904cb2cafe42a0824bd681396e
Dmitry029 <kdp.029029@cmail.com>
2025-05-27 19:48:16 +0300

    Add get employee by id

--- src/main/java/com/luv2code/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/luv2code/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -20,11 +20,26 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> findAll() {
-
         // create a query
         TypedQuery<Employee> query = entityManager.createQuery("from Employee ", Employee.class);
 
         //extract query and get result list
         return query.getResultList();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return entityManager.find(Employee.class, id);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return entityManager.merge(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee theEmployee = entityManager.find(Employee.class, id);
+        entityManager.remove(theEmployee);
     }
 }
--- src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.cruddemo.rest;
 import com.luv2code.cruddemo.entity.Employee;
 import com.luv2code.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,17 @@ public class EmployeeRestController {
     }
 
     @GetMapping("/employees")
-    public List<Employee> getEmployees() {
+    public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee is not found - " + employeeId);
+        }
+        return theEmployee;
     }
 }
