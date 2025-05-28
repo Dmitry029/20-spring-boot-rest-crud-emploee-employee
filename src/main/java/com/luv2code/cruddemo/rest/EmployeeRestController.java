c6183b785bf9f543b0ccc573b92fd364061fb9b8
Dmitry029 <kdp.029029@cmail.com>
2025-05-28 14:57:48 +0300

    Add patch employee

--- src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
@@ -1,19 +1,24 @@
 package com.luv2code.cruddemo.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.luv2code.cruddemo.entity.Employee;
 import com.luv2code.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
+    private final ObjectMapper objectMapper;
     private final EmployeeService employeeService;
 
-    public EmployeeRestController(EmployeeService employeeService) {
+    public EmployeeRestController(EmployeeService employeeService, ObjectMapper objectMapper) {
         this.employeeService = employeeService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/employees")
@@ -42,5 +47,29 @@ public class EmployeeRestController {
         return employeeService.save(theEmployee);
     }
 
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId,
+                                  @RequestBody Map<String, Object> patchPayload) {
 
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee is not found - " + employeeId);
+        }
+
+        //throw exception if request body contains "id" key
+        if (patchPayload.containsKey("id")) {
+            throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
+        }
+
+        Employee pathedEmployee = apply(patchPayload, tempEmployee);
+        return employeeService.save(pathedEmployee);
+    }
+
+    private Employee apply(Map<String, Object> patchPayload, Employee tempEmployee) {
+        ObjectNode employeeNode = objectMapper.convertValue(tempEmployee, ObjectNode.class);
+        ObjectNode patchNode = objectMapper.convertValue(patchPayload, ObjectNode.class);
+        employeeNode.setAll(patchNode);
+        return objectMapper.convertValue(employeeNode, Employee.class);
+    }
 }
